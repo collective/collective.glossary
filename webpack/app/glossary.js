6aff9a506cdc4edf79ebc775cd559a262f914d53
Nicolas Demonte <nicolas@affinitic.be>
2018-11-27 17:44:59 +0100

    Fix portal_url for Plone 5

--- webpack/app/glossary.js
+++ webpack/app/glossary.js
@@ -1,6 +1,8 @@
 import './js/tooltip';
 import './js/jquery.glossarize';
 
+var portal_url = (portal_url === undefined) ? $('body').attr('data-portal-url') : portal_url;
+
 $('#content-core').glossarizer({
     sourceURL: portal_url + '/@@glossary',
     callback: function() {
