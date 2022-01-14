987f7c58abf16560bac0cffbc524e2c902d65368
ksuess <k.suess@rohberg.ch>
2022-01-14 17:40:40 +0100

    We can Python 3.9.

--- setup.py
+++ setup.py
@@ -5,16 +5,18 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-long_description = '\n\n'.join([
-    open('README.rst').read(),
-    open('CONTRIBUTORS.rst').read(),
-    open('CHANGES.rst').read(),
-])
+long_description = "\n\n".join(
+    [
+        open("README.rst").read(),
+        open("CONTRIBUTORS.rst").read(),
+        open("CHANGES.rst").read(),
+    ]
+)
 
 
 setup(
-    name='collective.glossary',
-    version='1.0a1',
+    name="collective.glossary",
+    version="1.0a1",
     description="A Dexterity-based content type to define a glossary and its terms.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
@@ -25,44 +27,45 @@ setup(
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone CMS',
-    author='Simples Consultoria',
-    author_email='products@simplesconsultoria.com.br',
-    url='https://github.com/collective/collective.glossary',
+    keywords="Python Plone CMS",
+    author="Simples Consultoria",
+    author_email="products@simplesconsultoria.com.br",
+    url="https://github.com/collective/collective.glossary",
     project_urls={
-        'PyPI': 'https://pypi.python.org/pypi/collective.glossary',
-        'Source': 'https://github.com/collective/collective.glossary',
-        'Tracker': 'https://github.com/collective/collective.glossary/issues',
+        "PyPI": "https://pypi.python.org/pypi/collective.glossary",
+        "Source": "https://github.com/collective/collective.glossary",
+        "Tracker": "https://github.com/collective/collective.glossary/issues",
         # 'Documentation': 'https://collective.glossary.readthedocs.io/en/latest/',
     },
-    license='GPL version 2',
-    packages=find_packages('src', exclude=['ez_setup']),
-    namespace_packages=['collective'],
-    package_dir={'': 'src'},
+    license="GPL version 2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    namespace_packages=["collective"],
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.8",
     install_requires=[
-        'setuptools',
+        "setuptools",
         # -*- Extra requirements: -*-
-        'z3c.jbot',
-        'plone.api>=1.8.4',
-        'plone.restapi',
-        'plone.app.dexterity',
-        'PyICU'
+        "z3c.jbot",
+        "plone.api>=1.8.4",
+        "plone.restapi",
+        "plone.app.dexterity",
+        "PyICU",
     ],
     extras_require={
-        'test': [
-            'plone.app.testing',
+        "test": [
+            "plone.app.testing",
             # Plone KGS does not use this version, because it would break
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
-            'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            "plone.testing>=5.0.0",
+            "plone.app.contenttypes",
+            "plone.app.robotframework[debug]",
         ],
     },
     entry_points="""
