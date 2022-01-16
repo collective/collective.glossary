e3222912eaa1c459fff8465fa2b9df399b8fd1f1
ksuess <k.suess@rohberg.ch>
2022-01-16 14:09:35 +0100

    Update intitialization of new fields with some logging

--- src/collective/glossary/upgrades/v1_1/__init__.py
+++ src/collective/glossary/upgrades/v1_1/__init__.py
@@ -7,16 +7,20 @@ from plone.app.textfield.value import RichTextValue
 def updateRichtextDefinition(setup_tool):
     """Fill new definition field with old description field."""
 
+    logger.info("Fill new definition field with old description field.")
+
     brains = api.content.find(portal_type="Term")
     for brain in brains:
         obj = brain.getObject()
         if not obj.definition:
+            logger.info("Intitialize " + obj.id + " " + obj.description)
             obj.definition = RichTextValue(
                 f"<p>{obj.description}</p>",
                 mimeType="text/html",
                 outputMimeType="text/html",
             )
         else:
-            logger.info("found term definition. Do not overwrite. " + obj.definition.raw)
+            logger.info("Found term definition for " + obj.id + ". Do not overwrite. " + obj.definition.raw)
         obj.reindexObject()
-    logger.info("Fill new definition field with old description field.")
+    logger.info("Glossary term definitions initialized.")
+
