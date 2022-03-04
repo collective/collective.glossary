f47954aecf14c078880aa34523d6a55703374012
ksuess <k.suess@rohberg.ch>
2022-03-04 13:03:40 +0100

    Switch from css and js registry to viewlet with ressources

---
+++ src/collective/glossary/browser/viewlets/viewlets.py
@@ -0,0 +1,8 @@
+from plone.app.layout.viewlets import common as base
+
+import plone.api
+
+
+class CssJS(base.ViewletBase):
+    def portal_url(self):
+        return plone.api.portal.get().absolute_url()
