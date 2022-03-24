aa41a4a46ad1be5a8fa3ddfbee63f224d78a4cf8
ksuess <k.suess@rohberg.ch>
2022-03-24 19:34:05 +0100

    Revert "Merge branch 'master' of github.com:collective/collective.glossary"
    This reverts commit a4764663b3061e555452716277906a12b67921aa, reversing
    changes made to 40df94501ad9f80a2dfb392805619992f54c8433.

--- src/collective/glossary/browser/static/main.js
+++ src/collective/glossary/browser/static/main.js
@@ -2,15 +2,11 @@
 var portal_url = (portal_url === undefined) ? $('body').attr('data-portal-url') : portal_url;
 
 $(function(){
-
   $('#content-core').glossarizer({
     sourceURL: portal_url + '/@@glossary',
-    caseSensitive: true,
-    exactMatch: true,
-    callback: function(){      
-      // Callback fired after glossarizer finishes its job      
+    callback: function() {
+      // Callback fired after glossarizer finishes its job
       new tooltip();
-
     }
   });
 });
