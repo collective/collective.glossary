98d8deb1e95f6cf17944ffa9d5c3c882c9a3001f
Maurits van Rees <maurits@vanrees.org>
2023-10-23 11:21:05 +0200

    Fix missing import of DEFAULT_MAXIMUM_WITHOUT_AZ_TOOLBAR.

--- src/collective/glossary/browser/views.py
+++ src/collective/glossary/browser/views.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from collective.glossary.config import DEFAULT_MAXIMUM_WITHOUT_AZ_TOOLBAR
 from collective.glossary.interfaces import IGlossarySettings
 from functools import cached_property
 from plone import api
